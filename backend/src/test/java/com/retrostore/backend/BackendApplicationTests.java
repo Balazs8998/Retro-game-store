f93a2879cd5348351e470c70b5deae0af10e0dc2
Balazs8998 <balazsrehm89@gmail.com>
2026-07-19 11:59:30 +0200

    chore: initialize Spring Boot and Angular projects

---
+++ backend/src/test/java/com/retrostore/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retrostore.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
